9b3b5de1e7643e903947a311fa55f49da7ed1042
Akhil Sharma <2004akhil26sharma@gmail.com>
2023-07-15 13:16:28 +0530

    Login-done

--- client/src/app/api/users/login/route.ts
+++ client/src/app/api/users/login/route.ts
@@ -1,28 +1,46 @@
-import {connect} from '@/db/dbConfig';
+import { connect } from "@/db/dbConfig";
 import User from "@/models/userModel";
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 connect();
-export async function POST(request: NextRequest){
-    try {
-        const reqBody=await request.json(); // awiat is important
-        const {email, password}=reqBody;
-        console.log(reqBody);
-        // check if user already exist
-        const user = await User.findOne({ email});
-        if(!user){ 
-            return NextResponse.json({error: "User not found"},{status: 400});
-        }
-        // check if password is correct
-        const validPassword = await bcryptjs.compare(password, user.password);
-        if(!validPassword){
-            return NextResponse.json({error: "Invalid password"},{status: 400});
-        }
-        //create token data
-        const tokenData = {id: user._id,
-        username: user.username,
-    email: user.email };
-    } catch (error:any) {
-        return NextResponse.json({error:error.message},{status:500});
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json(); // awiat is important
+    const { email, password } = reqBody;
+    console.log(reqBody);
+    // check if user already exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 400 });
     }
+    // check if password is correct
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+    //create token data:
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    }; // a lot more than this data can be sent through the token
+    // create token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    }); //signed token
+    const response = NextResponse.json({
+      message: "Login Succefully",
+      success: true,
+    });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+    return response;
+  
+
+    } 
+  catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -4,23 +4,47 @@ import React from "react";
 // import Router from "next/router"; wrong
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     // username: "",
   });
+  const [buttonDisabled,setbuttonDisabled]=React.useState(false);
+  const [loading,setloading]=React.useState(false);
   const onLogin = async () => {
     // As this method is going to talk to database, it is async
+    try {
+      setloading(true);
+      const response=await axios.post("/api/users/login",user);
+      console.log("Login success",response.data);
+      toast.success("login success");
+      router.push("/profile");
+    } catch (error:any) {
+      console.log("Login failed",error.message);
+      toast.error(error.message);
+    }
+    finally {
+      setloading(false);}
   };
+  React.useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 7) {
+      setbuttonDisabled(false);
+    }
+    else{
+      setbuttonDisabled(true);
+    }
+  },[user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 p-3 content-evenly ">
-      <h1>Login</h1>
+      <h1>{loading ? "Loading" : "Login"}</h1>
       <hr />
       <label htmlFor="email" className="font-bold">email</label>
       <input
-        className="p-2"
+        className="p-2 text-black"
         type="email"
         id="email"
         value={user.email}
@@ -29,7 +53,7 @@ export default function LoginPage() {
       />
       <label htmlFor="password" className="font-bold">password</label>
       <input
-        className="p-2"
+        className="p-2 text-black"
         type="password"
         id="password"
         value={user.password}
@@ -39,7 +63,7 @@ export default function LoginPage() {
       <button 
         onClick={onLogin}
         >
-        Login
+        {buttonDisabled ? "no login" : "Login"}
       </button>
       Don't Have an account?
       <Link href="/signup">click here</Link>
